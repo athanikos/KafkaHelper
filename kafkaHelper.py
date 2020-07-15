de8aae378bdf0e4dff495b438a4c171f6fb9b391
nikos <athanikos@gmail.com>
2020-07-15 19:08:42 +0300

    test produce conume passes with group id = none and earliest

--- kafkaHelper.py
+++ kafkaHelper.py
@@ -1,8 +1,9 @@
-from json import dumps
+from json import dumps, loads
 from kafka import KafkaProducer
 from enum import Enum
 import jsonpickle
 from kafka import KafkaConsumer
+
 
 class Action(Enum):
     Added = 1
@@ -10,24 +11,34 @@ class Action(Enum):
     Deleted = 3
 
 
-def to_json_with_action(data_item, action):
+def with_action(data_item, action):
     if data_item is None:
         raise ValueError(" does not exist ")
-    setattr(data_item,'action', action)
-    item =  jsonpickle.encode(data_item)
-    print(item)
+    setattr(data_item, 'action', action)
+    return jsonpickle.encode(data_item)
 
 
-def produce_with_action(broker_names, topic,  data_item):
-    producer = KafkaProducer(bootstrap_servers=[broker_names],
+def produce_with_action(broker_names, topic, data_item, id):
+    producer = KafkaProducer(bootstrap_servers=broker_names,
                              value_serializer=lambda x:
                              dumps(x).encode('utf-8'))
-    producer.send(topic, to_json_with_action(data_item =data_item) )
+    producer.send(topic, data_item)
 
 
-def consume(broker_names, topic,  consumer_group):
-    consumer = KafkaConsumer('my_favorite_topic', group_id='my_favorite_group')
+def consume(broker_names, topic, consumer_group):
+    consumer = KafkaConsumer(
+        'testme',
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        group_id=consumer_group,
+        value_deserializer=lambda x: loads(x.decode('utf-8')))
+
     items = []
     for msg in consumer:
+        print(msg.value)
         items.append(msg)
+        break
     return items
+
+
--- tests/kafka_Helper_tests.py
+++ tests/kafka_Helper_tests.py
@@ -1,22 +1,26 @@
+import json
+
 DATE_FORMAT = '%Y-%m-%d'
-from kafkaHelper import to_json_with_action, Action, produce_with_action, consume
+from kafkaHelper import with_action, Action, produce_with_action, consume
 
 
 class TestOject():
-    def __init__(self):
-        self.id = 1
-        self.descr = "hi"
+
+        def toJSON(self):
+            return json.dumps(self, default=lambda o: o.__dict__,
+                sort_keys=True, indent=4)
 
 
 def test_to_json():
     my_obj = TestOject()
-    to_json_with_action(my_obj, action=Action.Added)
-    assert (my_obj.action, Action.Added)
+    with_action(my_obj, action=Action.Added)
+    assert (my_obj.action == Action.Added)
 
 
 def test_produce_to_kafka():
     my_obj = TestOject()
-    to_json_with_action(my_obj, action=Action.Added)
-    produce_with_action(broker_names=["localhost:9092"],topic="testme",data_item=my_obj)
-    items = consume(broker_names=["localhost:9092"],consumer_group="testme",topic="testme")
+    my_obj.id = 10
+    with_action(my_obj, action=Action.Added)
+    produce_with_action(broker_names=["localhost:9092"],topic="testme",data_item=json.dumps( {'hi' :'there'}   ), id=my_obj.id )
+    items = consume(broker_names=["localhost:9092"],consumer_group=None,topic="testme")
     assert (len(items) == 1)
