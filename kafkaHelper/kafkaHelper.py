6a817d2b4231f0e5e0752e12e2b6169c9cd80bec
nikos <athanikos@gmail.com>
2020-09-01 10:16:42 +0300

    synchronous produce key circle ci

--- kafkaHelper/kafkaHelper.py
+++ kafkaHelper/kafkaHelper.py
@@ -19,8 +19,7 @@ def produce(broker_names, topic, data_item):
 def produce_with_key(broker_names, topic, data_item, key):
     producer = KafkaProducer(bootstrap_servers=broker_names,
                              value_serializer=lambda x:
-                             dumps(x).encode('utf-8'),
-                             key=key
+                             dumps(x).encode('utf-8')
                              )
     future = producer.send(topic, value=data_item, key=key)
     try:
